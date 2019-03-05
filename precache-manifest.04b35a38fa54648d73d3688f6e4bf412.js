68150bf19b319148c59953fa2b4d1752cd8718ed
SuryaSrikanth <srisurya2606@gmail.com>
2019-03-05 23:56:53 +0530

    Updates

---
+++ precache-manifest.04b35a38fa54648d73d3688f6e4bf412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "98554640f1ac2b0bda5d",
+    "url": "/react-test/static/js/runtime~main.98554640.js"
+  },
+  {
+    "revision": "02c72a780c29303d5ba1",
+    "url": "/react-test/static/js/main.02c72a78.chunk.js"
+  },
+  {
+    "revision": "7384b8de0a093cd46a26",
+    "url": "/react-test/static/js/2.7384b8de.chunk.js"
+  },
+  {
+    "revision": "02c72a780c29303d5ba1",
+    "url": "/react-test/static/css/main.a2c91ea6.chunk.css"
+  },
+  {
+    "revision": "a0da10fc86f6b23f1778ad5e7e717919",
+    "url": "/react-test/index.html"
+  }
+];
